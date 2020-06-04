d2939c64dea3acd0d87cd1b4b984e195ee71351b
gekelund <gustafaekelund@gmail.com>
2020-06-04 15:00:46 +0200

    new version navbar

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,11 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction column;
+  border: 2px solid green;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 20px;
+  height: 100%;
+`;
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>hej</p>
-    </div>
+    <Container>
+      <h1>ABOUT ME</h1>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
+      </p>
+    </Container>
   );
 };
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  margin: 20px;
+  height: 100%;
+`;
 
 const Home = () => {
   return (
-    <div>
-      <p>hej</p>
-      <p>hello</p>
-    </div>
+    <Container>
+      <h1>I AM GUSTAF EKELUND</h1>
+      <h3>FRONT END DEVELOPER</h3>
+    </Container>
   );
 };
 
